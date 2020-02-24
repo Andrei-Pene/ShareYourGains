c71faa9af1a6ddb4314eb5c5dc8bacf3f6401dc1
Andrei Pene <tue19085@temple.edu>
2020-02-24 11:54:22 -0500

    heroku app

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ const server = new ApolloServer({
     
   });
 
-server.applyMiddleware({app, path:'/graphql', cors:false});
+server.applyMiddleware({app, path:'/graphql'});
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client','build', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client','build','index.html'));
   })
 
 }
